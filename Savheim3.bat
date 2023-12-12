e2c1febce8c31c00107c686bd3087fa57a374634
csavarese <chris.savarese@gmail.com>
2023-12-12 07:46:45 +0000

    startup script

--- Savheim3.bat
+++ Savheim3.bat
@@ -1,8 +1,13 @@
+:: Savheim III script for pulling updates from git and then launching the game.  You can create a shortcut to this on your desktop.
+
 :: The "cd" line below assumes you used the default install location for Valheim from Steam.  Update as needed if you used a different install location, or if you're a Goatman.
 cd C:\Program Files (x86)\Steam\steamapps\common\Valheim
 
-::git reset --hard origin/main
-::git pull
-::pause
+git reset --hard origin/main
+git pull
+
+:: Uncomment the following pause if you want to see updates before launching the game.
+pause
+
 start valheim.exe
 exit
