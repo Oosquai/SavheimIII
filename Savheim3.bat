7f65ba416e00d4aa6cca0d06272b198ea0c57ee3
csavarese <chris.savarese@gmail.com>
2023-12-12 07:39:46 +0000

    pull and start bat

---
+++ Savheim3.bat
@@ -0,0 +1,8 @@
+:: The "cd" line below assumes you used the default install location for Valheim from Steam.  Update as needed if you used a different install location, or if you're a Goatman.
+cd C:\Program Files (x86)\Steam\steamapps\common\Valheim
+
+::git reset --hard origin/main
+::git pull
+::pause
+start valheim.exe
+exit
