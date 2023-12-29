a1ab6e5314816caa27e50e5076f351c2248a526c
csavarese <chris.savarese@gmail.com>
2023-12-29 21:59:35 +0000

    ignoring configs with GUI

--- Savheim3_TotinoTunes.bat
+++ Savheim3_TotinoTunes.bat
@@ -5,26 +5,26 @@
 cd C:\Program Files (x86)\Steam\steamapps\common\Valheim
 
 :: The following lines, and their counterparts below git pull, preserve some local settings for you, like if you moved some GUI frames around in-game or adjusted coordinates for 3440x1440 resolutions, etc.
-cd BepInEx\config
-if exist Azumatt.AzuExtendedPlayerInventory.cfg move /y Azumatt.AzuExtendedPlayerInventory.cfg Azumatt.AzuExtendedPlayerInventory_SAVHEIM_TEMP.cfg
-if exist Azumatt.BowsBeforeHoes.cfg move /y Azumatt.BowsBeforeHoes.cfg Azumatt.BowsBeforeHoes_SAVHEIM_TEMP.cfg
-if exist Azumatt.FastLink.cfg move /y Azumatt.FastLink.cfg Azumatt.FastLink_SAVHEIM_TEMP.cfg
-if exist org.bepinex.plugins.darwinawards.cfg move /y org.bepinex.plugins.darwinawards.cfg org.bepinex.plugins.darwinawards_SAVHEIM_TEMP.cfg
-if exist org.bepinex.plugins.groups.cfg move /y org.bepinex.plugins.groups.cfg org.bepinex.plugins.groups_SAVHEIM_TEMP.cfg
-cd ..\..
+::cd BepInEx\config
+::if exist Azumatt.AzuExtendedPlayerInventory.cfg move /y Azumatt.AzuExtendedPlayerInventory.cfg Azumatt.AzuExtendedPlayerInventory_SAVHEIM_TEMP.cfg
+::if exist Azumatt.BowsBeforeHoes.cfg move /y Azumatt.BowsBeforeHoes.cfg Azumatt.BowsBeforeHoes_SAVHEIM_TEMP.cfg
+::if exist Azumatt.FastLink.cfg move /y Azumatt.FastLink.cfg Azumatt.FastLink_SAVHEIM_TEMP.cfg
+::if exist org.bepinex.plugins.darwinawards.cfg move /y org.bepinex.plugins.darwinawards.cfg org.bepinex.plugins.darwinawards_SAVHEIM_TEMP.cfg
+::if exist org.bepinex.plugins.groups.cfg move /y org.bepinex.plugins.groups.cfg org.bepinex.plugins.groups_SAVHEIM_TEMP.cfg
+::cd ..\..
 
 :: Pull all the latest Savheim III files.
 git reset --hard origin/main
 git pull
 
 :: Replace the pulled files with temp files if they were created
-cd BepInEx\config
-if exist Azumatt.AzuExtendedPlayerInventory_SAVHEIM_TEMP.cfg move /y Azumatt.AzuExtendedPlayerInventory_SAVHEIM_TEMP.cfg Azumatt.AzuExtendedPlayerInventory.cfg
-if exist Azumatt.BowsBeforeHoes_SAVHEIM_TEMP.cfg move /y Azumatt.BowsBeforeHoes_SAVHEIM_TEMP.cfg Azumatt.BowsBeforeHoes.cfg
-if exist Azumatt.FastLink_SAVHEIM_TEMP.cfg move /y Azumatt.FastLink_SAVHEIM_TEMP.cfg Azumatt.FastLink.cfg
-if exist org.bepinex.plugins.darwinawards_SAVHEIM_TEMP.cfg move /y org.bepinex.plugins.darwinawards_SAVHEIM_TEMP.cfg org.bepinex.plugins.darwinawards.cfg
-if exist org.bepinex.plugins.groups_SAVHEIM_TEMP.cfg move /y org.bepinex.plugins.groups_SAVHEIM_TEMP.cfg org.bepinex.plugins.groups.cfg
-cd ..\..
+::cd BepInEx\config
+::if exist Azumatt.AzuExtendedPlayerInventory_SAVHEIM_TEMP.cfg move /y Azumatt.AzuExtendedPlayerInventory_SAVHEIM_TEMP.cfg Azumatt.AzuExtendedPlayerInventory.cfg
+::if exist Azumatt.BowsBeforeHoes_SAVHEIM_TEMP.cfg move /y Azumatt.BowsBeforeHoes_SAVHEIM_TEMP.cfg Azumatt.BowsBeforeHoes.cfg
+::if exist Azumatt.FastLink_SAVHEIM_TEMP.cfg move /y Azumatt.FastLink_SAVHEIM_TEMP.cfg Azumatt.FastLink.cfg
+::if exist org.bepinex.plugins.darwinawards_SAVHEIM_TEMP.cfg move /y org.bepinex.plugins.darwinawards_SAVHEIM_TEMP.cfg org.bepinex.plugins.darwinawards.cfg
+::if exist org.bepinex.plugins.groups_SAVHEIM_TEMP.cfg move /y org.bepinex.plugins.groups_SAVHEIM_TEMP.cfg org.bepinex.plugins.groups.cfg
+::cd ..\..
 
 :: Plays background music as the game is loading
 cd sound
